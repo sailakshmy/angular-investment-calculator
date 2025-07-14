30e6af8b44813505e1300e2be9d68bccdfa4ec8a
Sai <sailakshmy94@gmail.com>
2025-07-14 08:29:10 +0400

    Created the User Input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
+import { UserInputComponent } from "./components/user-input/user-input.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   templateUrl: "./app.component.html",
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
 })
 export class AppComponent {}
