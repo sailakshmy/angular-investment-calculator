485d8dccf595e4a83d45f32bd0df6e930eaa28cf
Sai <sailakshmy94@gmail.com>
2025-07-14 08:46:46 +0400

    Used the calculateInvestmentResults function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
 import { UserInputComponent } from "./components/user-input/user-input.component";
+import { InputData } from "./app.model";
 
 @Component({
   selector: "app-root",
@@ -8,4 +9,32 @@ import { UserInputComponent } from "./components/user-input/user-input.component
   templateUrl: "./app.component.html",
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults({
+    initialInvestment,
+    duration,
+    expectedReturn,
+    annualInvestment,
+  }: InputData) {
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
---
+++ src/app/app.model.ts
@@ -0,0 +1,6 @@
+export interface InputData {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
